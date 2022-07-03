932f519b0aad79cc49adda3dbfb7bdec55345cfc
Elena Samoilenko <nuclear.cover@gmail.com>
2022-07-03 12:33:03 +0300

    homework 6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -1,40 +1,46 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
 
 function HW6() {
-    const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('')
 
-    const save = () => {
-        saveState<string>('editable-span-value', value)
-    }
-    const restore = () => {
-        // setValue()
-    }
 
-    return (
-        <div>
-            <hr/>
-            homeworks 6
+  //по клику сохрнаяю в lst.
+  const save = () => {
+    saveState<string>('editable-span-value', value)
+  }
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
-            </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+  // по клику послек обновления могу достать из lSt.Если не нажать save, то оно не улетит в lSt
+  const restore = () => {
+    setValue(restoreState<string>('editable-span-value', ''))
+  }
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
-        </div>
-    )
+
+  return (
+    <div>
+      <hr/>
+      <div className={'title__hw'}><span>HOMEWORK 6</span></div>
+
+      <div style={{margin: '5px'}}>
+        <SuperEditableSpan
+          value={value}
+          //меняет локальную переменную, но не меняет state
+          //если хочу на onChange, то через UseEffect
+          onChangeText={setValue}
+          // если value пустой, то как бы placeholder(children)
+          spanProps={{children: value ? undefined : 'Enter text...'}}
+        />
+      </div>
+      <SuperButton onClick={save}>save</SuperButton>
+      <SuperButton onClick={restore}>restore</SuperButton>
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperEditableSpan/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW6
